bc051df3938eb7c570533f3d25c1b823261d46cd
Natsuki Hasegawa <oosacker@gmail.com>
2021-05-20 23:08:15 +1200

    wip

---
+++ src/components/Photos.vue
@@ -0,0 +1,37 @@
+<template>
+  <h3>Here are my photos</h3>
+  <div class="photos">
+    <div 
+      v-for="photo in allPhotos"
+      :key="photo.id"
+      class="photo"
+    >
+      {{ photo.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Photos",
+  computed: {
+    allPhotos() {
+      return this.$store.state.photos
+    }
+  },
+  methods: {
+    fetchPhotos() {
+      this.$store.dispatch('fetchPhotos').then(() => {
+        console.log('fetched data OK')
+      })
+    }
+  },
+  created() {
+    this.fetchPhotos()
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,28 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    photos: []
   },
   mutations: {
+    setPhotos: (state, photos) => (state.photos = photos)
   },
   actions: {
+    fetchPhotos({ commit }) {
+      return new Promise((resolve, reject) => {
+        axios.get(`https://jsonplaceholder.typicode.com/photos`)
+        .then((response) => {
+          commit('setPhotos', response.data)
+          resolve()
+        })
+        .catch((error) => {
+          commit('setPhotos', [])
+          alert(error)
+          reject()
+        });
+      })
+    }
   },
   modules: {
   }
